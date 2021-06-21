1bff15a6e95d8ae4fdbd3c13af969ba6800293d1
itzomen <peng.akebuon2468@gmail.com>
2021-06-21 15:14:44 +0100

    Initialize first spring project

---
+++ src/main/java/com/omen/tsauth/TsauthApplication.java
@@ -0,0 +1,13 @@
+package com.omen.tsauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsauthApplication.class, args);
+	}
+
+}
